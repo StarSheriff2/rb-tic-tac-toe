b49e3cbd132e56b64319ac9205dc258f7a2bd43a
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-15 08:54:48 -0600

    simplify win method in game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,8 @@ class Game
   attr_accessor :game, :name, :players, :player_turn
   attr_reader :winner
 
+  WINNING_COMBINATIONS = [[1, 2, 3], [7, 8, 9], [1, 4, 7], [3, 6, 9], [4, 5, 6], [2, 5, 8], [1, 5, 9], [3, 5, 7]].freeze
+
   def initialize
     @winner = nil
     @game = Array.new(10)
@@ -38,10 +40,7 @@ class Game
 
   def win(moves, player)
     if moves.length > 2
-      permutations = moves.permutation(3).to_a
-      permutations = permutations.select { |el| el[1] > el[0] && el[1] < el[2] }
-      return @winner = player unless (permutations & [[1, 2, 3], [7, 8, 9], [1, 4, 7], [3, 6, 9],
-                                                      [4, 5, 6], [2, 5, 8], [1, 5, 9], [3, 5, 7]]).empty?
+      WINNING_COMBINATIONS.each { |combination| return @winner = player if (combination & moves) == combination }
     end
     @winner = 'draw' if @game.all?
   end
